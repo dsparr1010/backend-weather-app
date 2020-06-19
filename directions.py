36c62160692aa90023dba72c5588a335f7952d8a
dsparr <dsparr1010@gmail.com>
2020-06-19 17:39:14 -0500

    add Directions class

--- directions.py
+++ directions.py
@@ -1,6 +1,42 @@
+from dotenv import load_dotenv
+import click
+import requests
+import json
+import os
 
-class Directions():
-    def __init__(self, home_address, work_address):
+load_dotenv()
+OW_TOKEN = os.environ.get('OW_TOKEN')
+MQ_TOKEN = os.environ.get('MQ_TOKEN')
 
+class Directions:
+    def __init__(self, start_address, work_address):
+        self.start_address = start_address
+        self.work_address = work_address
         print('connected')
-        print(home_address)
+        click.echo(self.start_address) # prints address
+        
+    
+    def get_directions(self, start_address, work_address):
+        click.echo('in get_direct func')
+        MQ_res = requests.get(
+            f'http://www.mapquestapi.com/directions/v2/route?key={MQ_TOKEN}&from={self.start_address}&to={self.work_address}')
+        MQ_res.raise_for_status()
+        MQ_dictionary = MQ_res.json()
+        MQ_miles = MQ_dictionary['route']['distance']
+        toll_checker = MQ_dictionary['route']['hasTollRoad']
+        return f'It will take approximately xminutes to travel the {MQ_miles} to work.'
+        
+    def __str__(self):
+        return f'Stringified info on {self}'
+    
+    
+    @property
+    def to_address(self):
+        formatted_str = self.work_address.replace('+', ' ')
+        return f'To address: {formatted_str}'
+    
+    @property
+    def from_address(self):
+        formatted_str = self.start_address.replace(',', ', ')
+        return f'From address: {formatted_str}'
+    
--- hello.py
+++ hello.py
@@ -16,31 +16,38 @@ MQ_TOKEN = os.environ.get('MQ_TOKEN')
 @click.argument('name', default='guest')
 def cli(string, name):
     '''Welcome to My Day Planner'''
-    home_address = '3716 W Eddy St,Chicago,IL'
+    start_address = '3716 W Eddy St,Chicago,IL'
     work_address = '1500+West+Shure+Drive,+Arlington+Heights,+IL'
+    city = 'chicago'
     # name = input('What\'s your name?\n')
-    print(Directions(home_address, work_address))
-    city = input(f'Hello! What city would you like the weather for?').lower()
+    # city = input(f'Hello! What city would you like the weather for?').lower()
     try:
         response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OW_TOKEN}&units=imperial')
         response.raise_for_status()
         # make responses in json format to easily access data
         response_dictionary = response.json()
         main, description = response_dictionary['weather'][0]['main'], response_dictionary['weather'][0]['description']
-        #print(response_dictionary)
         current_temp, low_temp, high_temp = int(round(response_dictionary['main']['temp'])), int(round(response_dictionary['main']['temp_min'])), int(round(response_dictionary['main']['temp_max']))
         click.echo(f'The weather in {city} is currently described as : {main.lower()} with {description}\n'+
                    f"The current temperature is {current_temp}\N{DEGREE SIGN} with a low of {low_temp}\N{DEGREE SIGN} and a high of {high_temp}\N{DEGREE SIGN}"
                    )
         drive_day = input('Are you driving to work today?')
         if city == 'chicago' and drive_day == 'yes':
-            MQ_res = requests.get(
-                f'http://www.mapquestapi.com/directions/v2/route?key={MQ_TOKEN}&from={home_address}&to={work_address}')
+            newDir = Directions(start_address, work_address)
+            getDir = newDir.get_directions(start_address, work_address)
+            print(getDir)
+            print(newDir.to_address)
+            print(newDir.from_address)
             
-            MQ_res.raise_for_status()
-            MQ_dictionary = MQ_res.json()
-            MQ_miles = MQ_dictionary['route']['distance']
-            click.echo(f'It will take approximately xminutes to travel the {MQ_miles} to work.')
+            
+            
+            # MQ_res = requests.get(
+            #     f'http://www.mapquestapi.com/directions/v2/route?key={MQ_TOKEN}&from={start_address}&to={work_address}')
+            
+            # MQ_res.raise_for_status()
+            # MQ_dictionary = MQ_res.json()
+            # MQ_miles = MQ_dictionary['route']['distance']
+            # click.echo(f'It will take approximately xminutes to travel the {MQ_miles} to work.')
             
         # print(MQ_dictionary)
     
