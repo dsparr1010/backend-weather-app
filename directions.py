a6c5ec7fe632735935108eeb24674004b49bab39
dsparr <dsparr1010@gmail.com>
2020-06-19 18:51:49 -0500

    create sub class for handling traffic

--- directions.py
+++ directions.py
@@ -3,40 +3,53 @@ import click
 import requests
 import json
 import os
+from datetime import datetime
 
 load_dotenv()
 OW_TOKEN = os.environ.get('OW_TOKEN')
 MQ_TOKEN = os.environ.get('MQ_TOKEN')
 
+# to work URL
+# http://www.mapquestapi.com/traffic/v2/incidents?key=FjAT0vnJcQYYG8oJ9se6Mbayp8mASMC3&boundingBox=-87.71,41.94,-88.02,42.13
+
 class Directions:
     def __init__(self, start_address, work_address):
         self.start_address = start_address
         self.work_address = work_address
-        print('connected')
-        click.echo(self.start_address) # prints address
         
     
     def get_directions(self, start_address, work_address):
-        click.echo('in get_direct func')
         MQ_res = requests.get(
             f'http://www.mapquestapi.com/directions/v2/route?key={MQ_TOKEN}&from={self.start_address}&to={self.work_address}')
         MQ_res.raise_for_status()
         MQ_dictionary = MQ_res.json()
         MQ_miles = MQ_dictionary['route']['distance']
         toll_checker = MQ_dictionary['route']['hasTollRoad']
-        return f'It will take approximately xminutes to travel the {MQ_miles} to work.'
-        
-    def __str__(self):
-        return f'Stringified info on {self}'
+        formattedTime = MQ_dictionary['route']['formattedTime']
+        return f'It will take approximately {formattedTime} to travel the {MQ_miles} miles to {self.to_address}.'
     
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
     
     @property
     def to_address(self):
         formatted_str = self.work_address.replace('+', ' ')
-        return f'To address: {formatted_str}'
+        return formatted_str
     
     @property
     def from_address(self):
         formatted_str = self.start_address.replace(',', ', ')
-        return f'From address: {formatted_str}'
+        return formatted_str
+
+# incident/traffic API is specfic - must have boundingBox rounded to nearest hundredths place and all spaces removed
+# Example:boundingBox=39.95,-105.25,39.52,-104.71
+
+class Route_Information(Directions):
+    def __init__(self, start_address, work_address):
+        super().__init__(start_address, work_address)
+        print('In Route info class')
+        
     
--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 from operator import itemgetter
-from directions import Directions
+from directions import Directions, Route_Information
+from datetime import datetime
 import json
 import os
 import click
 import requests
+import geocoder
 
 load_dotenv()
 OW_TOKEN = os.environ.get('OW_TOKEN')
@@ -24,32 +26,27 @@ def cli(string, name):
     try:
         response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OW_TOKEN}&units=imperial')
         response.raise_for_status()
-        # make responses in json format to easily access data
         response_dictionary = response.json()
         main, description = response_dictionary['weather'][0]['main'], response_dictionary['weather'][0]['description']
         current_temp, low_temp, high_temp = int(round(response_dictionary['main']['temp'])), int(round(response_dictionary['main']['temp_min'])), int(round(response_dictionary['main']['temp_max']))
         click.echo(f'The weather in {city} is currently described as : {main.lower()} with {description}\n'+
                    f"The current temperature is {current_temp}\N{DEGREE SIGN} with a low of {low_temp}\N{DEGREE SIGN} and a high of {high_temp}\N{DEGREE SIGN}"
                    )
-        drive_day = input('Are you driving to work today?')
-        if city == 'chicago' and drive_day == 'yes':
-            newDir = Directions(start_address, work_address)
-            getDir = newDir.get_directions(start_address, work_address)
-            print(getDir)
-            print(newDir.to_address)
-            print(newDir.from_address)
-            
-            
-            
-            # MQ_res = requests.get(
-            #     f'http://www.mapquestapi.com/directions/v2/route?key={MQ_TOKEN}&from={start_address}&to={work_address}')
-            
-            # MQ_res.raise_for_status()
-            # MQ_dictionary = MQ_res.json()
-            # MQ_miles = MQ_dictionary['route']['distance']
-            # click.echo(f'It will take approximately xminutes to travel the {MQ_miles} to work.')
-            
-        # print(MQ_dictionary)
+        
+        
+        weekday = Directions.is_workday(datetime.date(datetime.now()))
+        if weekday:
+            drive_to_work = input('It\'s a weekday - Do you want to see the current traffic conditions to work?')
+            if drive_to_work == 'yes':
+                newDir = Directions(start_address, work_address)
+                getDir = newDir.get_directions(start_address, work_address)
+                print(getDir)
+                newRouteInfo = Route_Information(newDir.start_address, newDir.work_address)
+                print(newRouteInfo.to_address) # access to parent attr - print formatted address
+            else:
+                click.echo('add some other func that asks another question')
+                
+
     
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
